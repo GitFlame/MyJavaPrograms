293b737683c62fd5e2f0cf62b0e79e34e27db51a
Priyanshu Jha <73241432+GitFlame@users.noreply.github.com>
2022-04-13 02:01:47 +0530

    Add files via upload

---
+++ loader.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class loader
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=22,b=7,div;
+        String str="one";
+        String str2="Two";
+        div=a*b/b;
+        if(div<=22){
+            System.out.println(str);
+            
+        }
+        else{
+            System.out.println(str2);
+        }
+    }
+}
